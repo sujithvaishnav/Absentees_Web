8330aed0afd363e519115470c033a79ba81396ff
Adamkoda2306 <147936412+Adamkoda2306@users.noreply.github.com>
2024-08-11 17:16:31 +0530

    webpage

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, send_from_directory, redirect, url_for, flash
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return send_from_directory('','index.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return send_from_directory('','about.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return send_from_directory('','contact.html')
 
 if __name__ == '__main__':
     app.secret_key = 'supersecretkey'
